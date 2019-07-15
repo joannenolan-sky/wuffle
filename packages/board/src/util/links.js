cb54c9528e831eb960f163c9f8e04af9d593d633
Joanne Nolan <joanne.nolan@sky.uk>
2019-07-15 14:00:43 +0100

    Add changes from release 0.12.0 to our Wuffle
    As the most recent changes have been quite big, this branch was recreated with the currently deployed version and most of the changes from v0.12.0
    Changes not included: searching by issue number instead of opening in Github
    Changes included: show epics by tasks completed, toggle hid children, click on milestone to search

--- packages/board/src/util/links.js
+++ packages/board/src/util/links.js
@@ -1,23 +1,3 @@
-export function isOpenOrMergedPull(issue) {
-
-  const {
-    state,
-    merged
-  } = issue;
-
-  return (state === 'open') || merged;
-}
-
-export function isOpenPullOrMergedPull(issue) {
-  const {
-    state,
-    merged,
-    pull_request
-  } = issue;
-
-  return (pull_request && (state === 'open')) || merged;
-}
-
 export function isClosingLink(link) {
   const {
     type,
@@ -33,43 +13,3 @@ export function isClosedByLink(link) {
 
   return type === 'CLOSED_BY';
 }
-
-export function isLinkedTo(link) {
-  const {
-    type,
-  } = link;
-
-  return type === 'LINKED_TO';
-}
-export function isDependentOn(link) {
-  const {
-    type,
-  } = link;
-
-  return type === 'DEPENDS_ON';
-}
-
-export function isRequiredBy(link) {
-  const {
-    type,
-  } = link;
-
-  return type === 'REQUIRED_BY';
-}
-
-export function isParentOf(link) {
-  const {
-    type,
-  } = link;
-
-  return type === 'PARENT_OF';
-}
-
-export function isChildOf(link) {
-  const {
-    type,
-  } = link;
-
-  return type === 'CHILD_OF';
-}
-
